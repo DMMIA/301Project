a7763e632a9042393a9658eb988249a02d777dfc
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-12-18 23:13:17 +0000

    added test communication with server

--- suite-spotter-front-end/src/App.jsx
+++ suite-spotter-front-end/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Location from './components/Location';
+import Test from './components/Test';
 import './App.css'
 
 function App() {
   return (
     <>
       <Location />
+      <Test />
     </>
   )
 }
