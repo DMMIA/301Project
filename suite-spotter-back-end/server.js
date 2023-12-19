cc0ad60ab3dbf4c33c2087a67d81f3a350c049d7
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-12-19 19:56:16 +0000

    Added middleware for planes and rooms

--- suite-spotter-back-end/server.js
+++ suite-spotter-back-end/server.js
@@ -5,10 +5,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const carRoute = require('./routes/cars');
-const planeRoute = require('./routes/cars');
-const roomRoute = require('./routes/cars');
+const planeRoute = require('./routes/planes');
+const roomRoute = require('./routes/rooms');
 const weatherRoute = require('./routes/cars');
 const foodRoute = require('./routes/cars');
+
+const { auth } = require('express-oauth2-jwt-bearer');
+const { amadeus } = require('./auth/amadeus');
 
 // mongoose.connect(process.env.MONGODB_URI);
 // const db = mongoose.connection;
@@ -19,14 +22,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// const jwtCheck = auth({
+//     audience: process.env.AUTH_AUDIENCE,
+//     issuerBaseURL: process.env.AUTH_ISSUER_BASE_URL,
+//     tokenSigningAlg: process.env.AUTH_TOKEN_SIGNING_ALG,
+// });
 
-const PORT = process.env.PORT || 3000;
+// app.use(jwtCheck);
+
+app.use(async (req, res, next) => {
+    req.amadeus = amadeus;
+    next();
+});
 
 // app.use('/cars', carRoute);
-// app.use('/planes', planeRoute);
-// app.use('/rooms', roomRoute);
+app.use('/planes', planeRoute);
+app.use('/rooms', roomRoute);
 // app.use('/weather', weatherRoute);
 // app.use('/food', foodRoute);
+
+
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Server listening');
@@ -34,6 +51,11 @@ app.get('/', (req, res) => {
 
 app.get('/marco', (req, res) => {
     res.send('Polo');
-})
+});
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
