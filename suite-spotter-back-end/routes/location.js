41680831a41f0916917169c61dde21631c4a740a
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-12-19 23:47:14 +0000

    Updated routes with generic variables

--- suite-spotter-back-end/routes/location.js
+++ suite-spotter-back-end/routes/location.js
@@ -3,13 +3,15 @@ const axios = require('axios');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-
   try {
-    const city = req.query.city;
-    const API_KEY = process.env.LOCATION_API_KEY;
-    const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${city}&format=json`;
-    const response = await axios.get(API);
+    const amadeus = req.amadeus;
+    const { city, lat, long } = req.query;
+    const response = await amadeus.referenceData.locations.cities.get({
+      keyword: city
+    });
+    console.log(response.data);
     res.json(response.data);
+
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
