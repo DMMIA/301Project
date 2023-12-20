34ae6cc384981dcf853a78f30029cc7bf02902d8
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-12-20 01:45:55 +0000

    Added Accordion display

---
+++ suite-spotter-front-end/src/components/AccordionSection.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import CarRental from './services/CarRental';
+import Flight from './services/Flight';
+import Hotel from './services/Hotel';
+
+export default function AccordionSection(props) {
+  return (
+    <Accordion defaultActiveKey="0">
+      <Row>
+        <Col>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Hotel</Accordion.Header>
+            <Accordion.Body>
+              <Hotel />
+            </Accordion.Body>
+          </Accordion.Item>
+        </Col>
+        <Col>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Flight</Accordion.Header>
+            <Accordion.Body>
+              <Flight
+                updateAirportData={props.updateAirportData}
+              />
+            </Accordion.Body>
+          </Accordion.Item>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>Car Rental</Accordion.Header>
+            <Accordion.Body>
+              <CarRental />
+            </Accordion.Body>
+          </Accordion.Item>
+        </Col>
+        <Col>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>Accordion Item #3</Accordion.Header>
+            <Accordion.Body>
+              Content for Accordion Item #3
+            </Accordion.Body>
+          </Accordion.Item>
+        </Col>
+      </Row>
+    </Accordion>
+  );
+}
