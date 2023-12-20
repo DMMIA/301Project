4e38dc3d1f12ddb05da1150d906c45efcd501841
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-12-20 04:33:37 +0000

    AFleshed out Header component. Added link navigation. Added log in button

--- suite-spotter-front-end/src/components/Header.jsx
+++ suite-spotter-front-end/src/components/Header.jsx
@@ -1,13 +1,28 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+import AuthButtons from '../Auth/AuthButtons';
+
 export default function Header() {
-    <header>
-        <img alt="Hotel Room" className="hotel-image" />.      //Image here.
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">My Trips</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Activities</a></li>
-            </ul>
-        </nav>
-    </header>
+    return (
+        <Navbar bg="light" expand="lg">
+      <Navbar.Brand as={Link} to="/">
+        <img
+          src="hotel-image-url"
+          alt="Hotel Room"
+          className="hotel-image"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/trips">My Trips</Nav.Link>
+          <Nav.Link as={Link} to="/activities">Activities</Nav.Link>
+          <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+          <AuthButtons />
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+      );
 }
