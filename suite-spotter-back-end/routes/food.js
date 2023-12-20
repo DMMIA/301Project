801ae5f8de9654feff7b44167a513bdfd8634ead
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-12-20 05:25:23 +0000

    Added food endpoint

---
+++ suite-spotter-back-end/routes/food.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const { latitude, longitude } = req.query;
+    const amadeus = req.amadeus;
+    const response = await amadeus.referenceData.locations.pointsOfInterest.get({
+      latitude: latitude || 41.397158,
+      longitude: longitude || 2.160873,
+      radius: 1,
+      categories: 'RESTAURANT'
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+module.exports = router;
