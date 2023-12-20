e788d7673c0f364929e24ea1f3d116820c1e1783
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-12-20 05:25:41 +0000

    Adjusted parameters

--- suite-spotter-back-end/routes/poi.js
+++ suite-spotter-back-end/routes/poi.js
@@ -4,13 +4,13 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const { latitude, longitude, startPort, guests, checkIn, checkOut } = req.query;
+    const { latitude, longitude } = req.query;
     const amadeus = req.amadeus;
     const response = await amadeus.referenceData.locations.pointsOfInterest.get({
       latitude: latitude || 41.397158,
       longitude: longitude || 2.160873,
       radius: 1,
-      categories: 'SIGHTS,NIGHTLIFE,RESTAURANT,SHOPPING'
+      categories: 'SIGHTS,NIGHTLIFE,SHOPPING'
     });
     res.json(response.data);
   } catch (error) {
