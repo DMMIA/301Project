69b74f0936941e1db98d3f05b233610ccc124a4f
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-12-20 01:44:54 +0000

    Added Header component

---
+++ suite-spotter-front-end/src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    <header>
+        <img alt="Hotel Room" className="hotel-image" />.      //Image here.
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">My Trips</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Activities</a></li>
+            </ul>
+        </nav>
+    </header>
+}
