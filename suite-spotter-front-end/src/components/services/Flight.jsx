3f594ad0ed32b857a7cd7abdfa5e16fba0ed2bc2
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-12-21 00:07:52 +0000

    Updated props. Updated form values

--- suite-spotter-front-end/src/components/services/Flight.jsx
+++ suite-spotter-front-end/src/components/services/Flight.jsx
@@ -5,10 +5,14 @@ import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel';
 
 
-export default function Flight({ guests, checkIn, checkOut, updateAirportData,updateTrips }){
+export default function Flight({ formData, updateFormData, updateTrips }) {
   const [startAirport, setStartAirport] = useState('');
   const [endAirport, setEndAirport] = useState('');
   const [flights, setFlights] = useState([]);
+  const guests = formData.guests;
+  const checkIn = formData.checkIn;
+  const checkOut = formData.checkOut;
+
 
   const SERVER = import.meta.env.VITE_SERVER_URL
   const handleFlightSearch = async () => {
@@ -27,7 +31,12 @@ export default function Flight({ guests, checkIn, checkOut, updateAirportData,up
       });
 
       setFlights(response.data.flights);
-      updateAirportData({ startAirport, endAirport });
+      updateFormData({
+        airportData: {
+          startAirport,
+          endAirport
+        }
+      });
     } catch (error) {
       console.error('Flight Search Error:', error.message);
     }
@@ -41,13 +50,13 @@ export default function Flight({ guests, checkIn, checkOut, updateAirportData,up
   };
   return (
     <>
-     <Form>
+      <Form>
         <Form.Group controlId="startAirport">
           <Form.Label>Starting Airport</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter starting airport"
-            value={startAirport}
+            value={formData.airportData.startAirport}
             onChange={(e) => setStartAirport(e.target.value)}
           />
         </Form.Group>
@@ -57,7 +66,7 @@ export default function Flight({ guests, checkIn, checkOut, updateAirportData,up
           <Form.Control
             type="text"
             placeholder="Enter ending airport"
-            value={endAirport}
+            value={formData.airportData.endAirport}
             onChange={(e) => setEndAirport(e.target.value)}
           />
         </Form.Group>
