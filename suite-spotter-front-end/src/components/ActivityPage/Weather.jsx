8faa0dc03284caad460a47a2bfdf210ab7408948
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-12-21 00:08:32 +0000

    Updated weather props

--- suite-spotter-front-end/src/components/ActivityPage/Weather.jsx
+++ suite-spotter-front-end/src/components/ActivityPage/Weather.jsx
@@ -1,20 +1,22 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 
-const Weather = (weatherData, key) => {
+const Weather = (weatherData) => {
   if(!weatherData) {
     return null;
   }
    return (
     <ListGroup>
       <ListGroup.Title>Weather</ListGroup.Title>
-      <ListGroup.Item key={key}>
-      {weatherData.weather.date}
-      {weatherData.weather.description}
-      </ListGroup.Item>
-      <ListGroup.Item></ListGroup.Item>
-      <ListGroup.Item></ListGroup.Item>
-      <ListGroup.Item></ListGroup.Item>
-
+      {weatherData.data.map((day, index) => (
+        <ListGroup.Item key={index}>
+          <strong>Date:</strong> {day.valid_date}<br />
+          <strong>High Temp:</strong> {day.max_temp}&deg;C<br />
+          <strong>Low Temp:</strong> {day.min_temp}&deg;C<br />
+          <strong>Precipitation:</strong> {day.precip}mm<br />
+          <strong>Description:</strong> {day.weather.description}<br />
+          <strong>Wind Speed:</strong> {day.wind_spd}m/s
+        </ListGroup.Item>
+      ))}
     </ListGroup>
   );
 }
