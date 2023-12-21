574344584b0353e7350e76977df84fbe3a9b47bd
Michelangelo Ascalon <mikeascalon@yahoo.com>
2023-12-20 21:17:01 -0800

    added accordion on the booking component

--- suite-spotter-front-end/src/components/myTrips/BookingComponent.jsx
+++ suite-spotter-front-end/src/components/myTrips/BookingComponent.jsx
@@ -1,60 +1,72 @@
 
-
 import { Card, Row, Col } from 'react-bootstrap';
+import Accordion from 'react-bootstrap/Accordion';
 
 const BookingComponent = ({ trips }) => {
-    return (
-      <div>
+  console.log(trips[0])
+  return (
+    <div>
+      <Accordion defaultActiveKey="0">
         {trips.map((trip, index) => (
-          <Card key={index} className="mb-3">
-            <Card.Body>
-              <Card.Title>{trip.destination}</Card.Title>
-              <Row>
-                <Col md={4}>
-                  <Card.Text>
-                    {trip.hotel && (
-                      <>
-                        <strong>Hotel:</strong> {trip.hotel}
-                        <br />
-                        <strong>Check-in:</strong> {trip.checkInDate}
-                        <br />
-                        <strong>Check-out:</strong> {trip.checkOutDate}
-                      </>
-                    )}
-                  </Card.Text>
-                </Col>
-                <Col md={4}>
-                  <Card.Text>
-                    {trip.carRental && (
-                      <>
-                        <strong>Car Rental:</strong> {trip.carRental}
-                        <br />
-                        <strong>Pick-up:</strong> {trip.carPickUpDate}
-                        <br />
-                        <strong>Drop-off:</strong> {trip.carDropOffDate}
-                      </>
-                    )}
-                  </Card.Text>
-                </Col>
-                <Col md={4}>
-                  <Card.Text>
-                    {trip.planeTicket && (
-                      <>
-                        <strong>Plane Ticket:</strong> {trip.planeTicket}
-                        <br />
-                        <strong>Departure:</strong> {trip.departureDate}
-                        <br />
-                        <strong>Return:</strong> {trip.returnDate}
-                      </>
-                    )}
-                  </Card.Text>
-                </Col>
-              </Row>
-            </Card.Body>
-          </Card>
+          <Accordion.Item key={index} eventKey="0">
+            <Accordion.Header>{trip.destination}</Accordion.Header>
+            <Accordion.Body>
+              {trip.hotelData && (
+
+                <>
+                  {/* <strong>Hotel:</strong> {trip.hotel}
+<br /> */}
+                  <strong>Beds:</strong> {trip.hotelData.beds}
+                  <br />
+                  <strong>Check-in:</strong> {trip.checkIn}
+                  <br />
+                  <strong>Check-out:</strong> {trip.checkOut}
+                  <br />
+                  <strong>Price:</strong> {`${trip.hotelData.price} ${trip.hotelData.currency}`}
+                  <br />
+
+                  {trip.carRentalData && (
+                    <>
+                      <strong>Car Rental:</strong> {trip.carRentalData.provider}
+                      <br />
+                      <strong>Pick-up:</strong> {trip.carPickUpDate}
+                      <br />
+                      <strong>Drop-off:</strong> {trip.carDropOffDate}
+                      <br />
+                      <img src={trip.carRentalData.imageURL} />
+                      <br />
+                    </>
+                  )}
+                  {trip.airportData && (
+                    <>
+                      <strong>Flight Number:</strong> {trip.airportData.flightNumber}
+                      <br />
+                      <strong>Departure:</strong> {trip.airportData.startDate}
+                      <br />
+                      <strong>Return:</strong> {trip.airportData.endDate}
+                      <br />
+                      <strong>From:</strong> {trip.airportData.startingAirport}
+                      <strong>  Arrive:</strong> {trip.airportData.endingAirport}
+                      <br />
+                      <strong>Price</strong> {`${trip.airportData.price}${trip.airportData.currency}`}
+                      <br />
+
+                    </>
+                  )}
+
+                </>
+              )}
+
+
+            </Accordion.Body>
+          </Accordion.Item>
         ))}
-      </div>
-    );
-  };
-  
-  export default BookingComponent;
+      </Accordion>
+    </div>
+  );
+};
+
+
+
+
+export default BookingComponent;
